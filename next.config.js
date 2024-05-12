d7db2fd1cba79b65de1cbdccf2fd2e57b91c52f8
razmans <razmans@gmail.com>
2024-05-12 22:54:41 +0800

    Exposing clockface

--- next.config.js
+++ next.config.js
@@ -1,10 +1,37 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import("./src/env.js");
+// const {
+//         withModuleFederation,
+//     } = require("@module-federation/nextjs-mf");
 
-/** @type {import("next").NextConfig} */
-const config = {};
+import withModuleFederation from "@module-federation/nextjs-mf";
+module.exports = {
+  future: { webpack5: true },
+  images: {
+    domains: ["upload.wikimedia.org"],
+  },
+  webpack: (
+    /** @type {{ output: { libraryTarget: any; }; cache: boolean; }} */ config,
+    /** @type {{ isServer: any; }} */ options,
+  ) => {
+    const { isServer } = options;
+    const mfConf = {
+      mergeRuntime: true, //experimental
+      name: "clockFace",
+      library: {
+        type: config.output.libraryTarget,
+        name: "clockFace",
+      },
+      filename: "static/runtime/clockFaceremoteEntry.js",
+      remotes: {},
+      exposes: {
+        "./Clock": "./_components/Clock",
+      },
+    };
+    config.cache = false;
+    new withModuleFederation({
+      ...config,
+      extraOptions: {},
+    });
 
-export default config;
+    return config;
+  },
+};
